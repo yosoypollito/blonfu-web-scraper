275c826218e84d477dee5b39aee600f7372bd829
Daniel Iglesias <dev.daif@gmail.com>
2023-10-02 11:49:43 -0400

    fix: In case of article not found, now deletes header and save rest html

--- src/getPostArticle.js
+++ src/getPostArticle.js
@@ -1,7 +1,12 @@
+
 export default function (html) {
-  const postArticle = html.querySelector('article');
+  const postArticle = html.querySelector('article.post');
   if (!postArticle) {
-    return null;
+    // This is a second method in case the article is not found
+    const headerNav = html.querySelector('.site-header');
+    headerNav.remove()
+
+    return html.innerHTML;
   }
 
   return postArticle.innerHTML;
